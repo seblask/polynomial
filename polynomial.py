b8ce2d80e533c870f7f2e80601ec237f39c208ff
Sebastian <sebastian.laskowski@voicelab.pl>
2018-05-16 20:22:32 +0200

    delete csv module; add parse of text file with context manager

--- polynomial.py
+++ polynomial.py
@@ -1,25 +1,22 @@
 #-*- coding: utf-8 -*-
 
 import numpy as np
-import csv
 
-class Train():
-
-    def main(self):
-        self.read_csv_data()
+class ReadCSV():
 
     def set_csv_file_path(self, csv_file):
         self.csv_file = csv_file
         return (csv_file)
 
-    def read_csv_data(self):
+    # Read CSV file and create two vectors: points x and points y
+    def read_csv_data(self, csv_file):
         vetor_points_x = []
         vetor_points_y = []
-        with open(self.csv_file, 'r') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',')
-            for row in spamreader:
-                vetor_points_x.append(float(row[0]))
-                vetor_points_y.append(float(row[1]))
+        with open(csv_file, 'r') as csvfile:
+            for line in csvfile:
+                x, y = line.strip().split(',')
+                vetor_points_x.append(float(x))
+                vetor_points_y.append(float(y))
         return (vetor_points_x, vetor_points_y)
 
 
@@ -28,9 +25,10 @@ class Train():
 
 def main():
     csv_file_path = 'ai-task-132.csv'
-    train = Train()
-    train.set_csv_file_path(csv_file_path)
-    vetor_points_x, vetor_points_y = train.read_csv_data()
+    readcsv = ReadCSV()
+    csv_file = readcsv.set_csv_file_path(csv_file_path)
+    vetor_points_x, vetor_points_y = readcsv.read_csv_data(csv_file=csv_file)
+    # print(vetor_points_x, vetor_points_y)
 
 if __name__ == "__main__":
     main()
